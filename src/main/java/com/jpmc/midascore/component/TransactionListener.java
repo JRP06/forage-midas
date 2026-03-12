081c3c7fc324c502c8ce3a16066b7fa5226e21fa
Josue Reyes <110793591+JRP06@users.noreply.github.com>
2026-03-12 02:00:57 -0500

    validate incoming Kafka transactions and save valid transactions to H2 database with updated sender and recipient balances

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -1,21 +1,37 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TransactionListener {
-    /*
-    what this class does?
-    - Watch the trader-updates topic
-    - When a message arrives, wake up
-    - Print it out so your debugger can see it
-     */
-    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core-group")
-    public void handleTransaction(Transaction transaction){
-        //transaction has arrrived here
-        System.out.println(transaction);
+
+    private final DatabaseConduit databaseConduit;
+
+    public TransactionListener(DatabaseConduit databaseConduit){
+        this.databaseConduit = databaseConduit;
     }
 
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core-group")
+    public void handleTransaction(Transaction transaction){
+        //find sender and recipient
+        UserRecord sender = databaseConduit.findUserById(transaction.getSenderId());
+        UserRecord recipient = databaseConduit.findUserById(transaction.getRecipientId());
+
+        //validate
+        if (sender == null || recipient == null) return;
+        if (sender.getBalance() < transaction.getAmount()) return;
+
+        //update balance
+        sender.setBalance(sender.getBalance() - transaction.getAmount());;
+        recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+
+        //save everything
+        databaseConduit.save(sender);
+        databaseConduit.save(recipient);
+        databaseConduit.save(new TransactionRecord(sender, recipient, transaction.getAmount()));
+    }
 }
